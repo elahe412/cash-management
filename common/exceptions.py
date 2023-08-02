e95d9d0086f75e255eef65b26c497ab4dc83bdd0
elahe412 <67527897+elahe412@users.noreply.github.com>
2023-08-02 22:43:35 +0330

    Update exceptions.py

--- common/exceptions.py
+++ common/exceptions.py
@@ -4,5 +4,4 @@ from rest_framework.exceptions import APIException
 
 class PermissionException(APIException):
     status_code = status.HTTP_403_FORBIDDEN
-    # default_detail = PermissionMsg.PERMISSION_DENIED
     default_code = 'permission_denied'
